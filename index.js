4d3fe9582b78a3637723e6ddc9355fbea43045d6
L-Greco <makaronaskonstandinos@gmail.com>
2021-04-12 17:28:55 +0300

    made  the 18th

--- index.js
+++ index.js
@@ -138,23 +138,59 @@ const typeOfAngles = function (n1) {
 // 13)
 
 // Create a function to find the index of the greatest element of a given array of integers
-
+const findingTheIndexOfTheGreatestElement = function (arr) {
+    let max = 0
+    let indexOfMax
+    for (i=0;i<arr.length;i++) {
+        if ( arr[i]>max) {
+            max = arr[i]
+            indexOfMax = i
+        }
+    }
+    return indexOfMax
+}
+// console.log(findingTheIndexOfTheGreatestElement([15469,21879635,2157487963,235,625894,7458963,21456]))
 // 14)
 
 // Create a function to get the largest even number from an array of integers.
-
+const findingTheBiggestEvenInAnArray = function (arr) {
+    let max =0
+    for(i=0;i<arr.length;i++) {
+        if (arr[i] % 2 === 0){
+            max = arr[i]
+        } else { max = " there are no even numbers here"}
+    } return max
+}
+// console.log(findingTheBiggestEvenInAnArray([1,3,5,235,7,9,345,0]))
 // 15)
 
 // Create a function to check from two given integers, whether one is positive and another one is negative.
-
+const integersCheck = function (n1,n2) {    
+                    return (n1 >=0 && n2 < 0) || (n1<0 && n2 >=0)
+}
+// console.log(integersCheck(-2,-9))
 // 16)
 
 // Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
-
+const uppercaseIfMoreThanThree = function (str) {
+    let newString
+    if ( str.length<3) {
+         newString = str.toUpperCase() }
+         else {
+             newString = str.slice(0,3).toLowerCase()+str.slice(3,str.length).toUpperCase()
+         } return newString
+    }
+// console.log(uppercaseIfMoreThanThree("ABC"))
 // 17)
 
 // Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
-
+const sumWithinRange = function (n1,n2) {
+    let sum = n1 + n2
+    if ( sum >50 && sum <80) {
+        return 65
+    }else { return 80}
+}
+// console.log ( sumWithinRange(23,33))
 // 18)
 
 // Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
@@ -170,7 +206,34 @@ const typeOfAngles = function (n1) {
 // this would be a "DiegoRiccardo".
 // 34 has four factors: 1, 2, 17, and 34.
 // this would be "34".
+const convertNumberToString = function (n1) {
+    let newString =""  //
+    let factor1 = 3
+    let factor2 = 5
+    let factor3 = 7
+    
+    for ( i=0;i<3;i++) {
+        if ( Math.abs(n1%3)===0 && factor1 === 3) {
+            newString = newString+"Diego"
+            factor1 = 0
+        } else if ( Math.abs(n1%5)===0 && factor2 === 5) {
+            newString= newString+" "+"Riccardo"
+            factor2 = 0
+        }else if (Math.abs(n1%7)===0 && factor3=== 7) {
+            newString= newString+" "+"Stefano"
+            factor3 = 0
+        } 
+     } 
+     if (factor1 === 0 || factor2 === 0 || factor3 === 0) {
+         return newString
+     } else { return `${n1}`}
+       
+     
+}
+console.log(convertNumberToString(21))
 
 // 19)
 // Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
-
+const acronym = function (str) {
+    
+}
